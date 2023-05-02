f991ab2472401f790b07f95fc1f1095a095e3ac1
TemaGolovin <golart1996@mail.ru>
2023-05-02 10:04:38 +0700

    add locales

---
+++ src/locales/ru.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    feeds: 'Фиды',
+    posts: 'Посты',
+    sucsess: 'RSS успешно загружен',
+    view: 'Просмотр',
+    errors: {
+      unvalidUrl: 'Ссылка должна быть валидным URL',
+      doubleRss: 'RSS уже существует',
+      Network: 'Ошибка сети',
+      unvalidRssResourse: 'Ресурс не содержит валидный RSS',
+    },
+  },
+};
