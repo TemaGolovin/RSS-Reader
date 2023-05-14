92e26019eed228e577f8f45b1f02e4f6675f4ff1
TemaGolovin <golart1996@mail.ru>
2023-05-14 16:29:20 +0700

    fix lint

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 // const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
+// eslint-disable-next-line no-underscore-dangle
 const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
 const __dirname = dirname(__filename);
 
 const config = {
