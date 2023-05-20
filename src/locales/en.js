c73099f11f067663245c7a786ae54ceecfa9daa6
TemaGolovin <golart1996@mail.ru>
2023-05-20 17:02:39 +0700

    add english locale

---
+++ src/locales/en.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    feeds: 'Feeds',
+    posts: 'Posts',
+    sucsess: 'RSS successfully uploaded',
+    view: 'View',
+    errors: {
+      unvalidUrl: 'The link must be a valid URL',
+      doubleRss: 'RSS already exists',
+      Network: 'Network error',
+      unvalidRssResourse: 'Resource does not contain valid RSS',
+      emptyField: 'Must not be empty',
+    },
+  },
+};
