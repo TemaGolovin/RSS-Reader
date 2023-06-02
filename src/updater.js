c96990b2d4c5b7082df46bb1c04a7b83818058fc
TemaGolovin <golart1996@mail.ru>
2023-06-02 09:16:14 +0700

    add PromiseAll

--- src/updater.js
+++ src/updater.js
@@ -1,53 +1,37 @@
-import axios from 'axios';
 import parser from './parser.js';
 
-const renderReadedPosts = (readedPostsId, ulPosts) => {
-  if (readedPostsId.length === 0) {
-    return;
-  }
-  readedPostsId.forEach((idPost) => {
-    const readedPost = ulPosts.querySelector(`[data-id='${idPost}']`);
-    if (!readedPost) {
-      return;
-    }
-    readedPost.classList.remove('fw-bold');
-    readedPost.classList.add('fw-normal', 'link-secondary');
-  });
-};
-
-const updatePosts = (state, proxyUrl, id, generateIdFunc) => axios
-  .get(proxyUrl)
-  .then((response) => response.data.contents)
-  .then((data) => {
-    const content = parser(data, generateIdFunc);
+const updatePosts = (state, getResponseRRS, generateIdFunc) => {
+  // eslint-disable-next-line max-len
+  const promises = state.resultContentLoding.feeds.map((feed) => getResponseRRS(feed.feedLink).then((response) => {
+    const content = parser(
+      response.data.contents,
+      generateIdFunc,
+      feed.feedLink,
+    );
     const { posts } = content;
     if (!posts) {
-      throw new Error('Parser Error');
+      return;
     }
-    return posts;
-  })
-  .then((requestedPosts) => {
+    let newPosts = [];
     const loadedPosts = state.resultContentLoding.posts.filter(
-      (post) => post.feedId === id,
+      (post) => post.feedId === feed.id,
     );
     const loadedPostsGuids = loadedPosts.map((post) => post.guid);
     const coll = new Set(loadedPostsGuids);
-    const newPosts = requestedPosts.filter(({ guid }) => !coll.has(guid));
-
+    newPosts = posts.filter(({ guid }) => !coll.has(guid));
     if (newPosts.length === 0) {
       return;
     }
     newPosts.forEach((post) => {
-      post.feedId = id;
-      post.id = generateIdFunc(); // eslint-disable-line no-param-reassign
+      post.feedId = feed.id;
+      post.id = generateIdFunc();
     });
     state.resultContentLoding.posts.push(...newPosts);
-
-    const ulPosts = document.querySelector('.posts ul');
-    renderReadedPosts(state.uiState.readedPostsId, ulPosts);
     return newPosts; // eslint-disable-line consistent-return
-  })
-  .catch((e) => console.error(e.message))
-  .finally(() => setTimeout(() => updatePosts(state, proxyUrl, id, generateIdFunc), 5000));
+  }));
+  Promise.all(promises)
+    .catch((e) => console.error(e.message))
+    .finally(() => setTimeout(() => updatePosts(state, getResponseRRS, generateIdFunc), 5000));
+};
 
-export { updatePosts, renderReadedPosts };
+export default updatePosts;
